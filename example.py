0b874c7d723533b3ad290e8123ca0645d922f0c0
Shad Carter <49698035+shadcarter@users.noreply.github.com>
2019-04-23 16:18:43 -0600

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 # import packages test
-from src.Garch import Garch
-from src.Garch import vanilla_garch, gjr_garch
+from Garch.Garch import Garch
+from Garch.Garch import vanilla_garch, gjr_garch
 import pandas as pd
 import numpy as np
 
